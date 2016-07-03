502a2aeba382cad30655d84595a3bfa1eb32cbbe
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-07-03 20:30:03 +0800

    move constant variable to another file.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- gas.go
+++ gas.go
@@ -99,50 +99,6 @@ import (
 	"sync"
 )
 
-const (
-	//-------------
-	// Media types
-	//-------------
-
-	ApplicationJSON                  = "application/json"
-	ApplicationJSONCharsetUTF8       = ApplicationJSON + "; " + CharsetUTF8
-	ApplicationJavaScript            = "application/javascript"
-	ApplicationJavaScriptCharsetUTF8 = ApplicationJavaScript + "; " + CharsetUTF8
-	ApplicationXML                   = "application/xml"
-	ApplicationXMLCharsetUTF8        = ApplicationXML + "; " + CharsetUTF8
-	ApplicationForm                  = "application/x-www-form-urlencoded"
-	ApplicationProtobuf              = "application/protobuf"
-	ApplicationMsgpack               = "application/msgpack"
-	TextHTML                         = "text/html"
-	TextHTMLCharsetUTF8              = TextHTML + "; " + CharsetUTF8
-	TextPlain                        = "text/plain"
-	TextPlainCharsetUTF8             = TextPlain + "; " + CharsetUTF8
-	MultipartForm                    = "multipart/form-data"
-
-	//---------
-	// Charset
-	//---------
-
-	CharsetUTF8 = "charset=utf-8"
-
-	//---------
-	// Headers
-	//---------
-
-	AcceptEncoding     = "Accept-Encoding"
-	Authorization      = "Authorization"
-	ContentDisposition = "Content-Disposition"
-	ContentEncoding    = "Content-Encoding"
-	ContentLength      = "Content-Length"
-	ContentType        = "Content-Type"
-	Location           = "Location"
-	Upgrade            = "Upgrade"
-	Vary               = "Vary"
-	WWWAuthenticate    = "WWW-Authenticate"
-	XForwardedFor      = "X-Forwarded-For"
-	XRealIP            = "X-Real-IP"
-)
-
 var defaultConfig = map[interface{}]interface{}{
 	"Mode":       "DEV",
 	"ListenAddr": "localhost",
