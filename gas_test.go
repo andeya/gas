1145e28ddf09be7b87fa724ba9c3dcc654e5b446
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-07-09 21:56:22 +0800

    Support serving HTTPS (secure) requests.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- gas_test.go
+++ gas_test.go
@@ -5,13 +5,14 @@ import (
 	"github.com/go-gas/gas/model/MySQL"
 	"github.com/stretchr/testify/assert"
 	"github.com/valyala/fasthttp"
+	"io/ioutil"
 	"net/http"
 	"testing"
+	"time"
 )
 
 var (
-	indexString      = "indexpage"
-	testStaticString = "This is a static file"
+	indexString = "indexpage"
 )
 
 func newHttpExpect(t *testing.T, h fasthttp.RequestHandler) *httpexpect.Expect {
@@ -30,7 +31,21 @@ func newHttpExpect(t *testing.T, h fasthttp.RequestHandler) *httpexpect.Expect {
 	return e
 }
 
-func Testgas(t *testing.T) {
+func testRequest(t *testing.T, url string) {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	assert.NoError(t, err)
+
+	_, ioerr := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, ioerr)
+	assert.Equal(t, "200 OK", resp.Status, "should get a 200")
+}
+
+func indexPage(ctx *Context) error {
+	return ctx.STRING(http.StatusOK, indexString)
+}
+
+func TestGas(t *testing.T) {
 	// new gas
 	g := New("testfiles/config_test.yaml")
 
@@ -44,8 +59,36 @@ func Testgas(t *testing.T) {
 		Body().Equal(indexString)
 }
 
-func indexPage(ctx *Context) error {
-	return ctx.STRING(http.StatusOK, indexString)
+func TestRun(t *testing.T) {
+	g := New()
+
+	// set route
+	g.Router.Get("/", indexPage)
+
+	go func() {
+		assert.NoError(t, g.Run())
+	}()
+	// have to wait for the goroutine to start and run the server
+	// otherwise the main thread will complete
+	time.Sleep(5 * time.Millisecond)
+
+	testRequest(t, "http://localhost:8080")
+}
+
+func TestRunTLS(t *testing.T) {
+	g := New()
+
+	// set route
+	g.Router.Get("/", indexPage)
+
+	go func() {
+		assert.NoError(t, g.RunTLS(":8081", "certificate/localhost.cert", "certificate/localhost.key"))
+	}()
+	// have to wait for the goroutine to start and run the server
+	// otherwise the main thread will complete
+	time.Sleep(5 * time.Millisecond)
+
+	testRequest(t, "https://localhost:8081")
 }
 
 func TestGas_NewModel(t *testing.T) {
@@ -70,7 +113,6 @@ func BenchmarkGas(b *testing.B) {
 	req := fasthttp.Request{}
 	req.SetRequestURI("/")
 	req.Header.SetMethod("GET")
-
 
 	for i := 0; i < b.N; i++ {
 		ctx := fasthttp.RequestCtx{
